caefbd09e6c37779381f2dac42b3aa66e9a26692
StephenWright <stevewright54@gmail.com>
2020-08-22 18:26:52 +0100

    update form with better validation

--- src/components/addRestaurant/AddRestaurant.jsx
+++ src/components/addRestaurant/AddRestaurant.jsx
@@ -2,21 +2,9 @@ import React, { useState } from 'react';
 import { addRestaurant } from '../api';
 
 const AddRestaurant = ({ auth }) => {
-  const [mustEat, setMustEat] = useState({
-    meOne: '',
-    meTwo: '',
-    meThree: '',
-  });
-  const [restaurantForm, setRestaurantForm] = useState({
-    name: '',
-    website: '',
-    postcode: '',
-    comment: '',
-    cuisine: '',
-    city: '',
-    budget: [],
-  });
-  const [error, setError] = useState(false);
+  const [mustEat, setMustEat] = useState({});
+  const [restaurantForm, setRestaurantForm] = useState({});
+  const [error, setError] = useState({});
 
   const handleInputChange = (e) => {
     const { value, name } = e.target;
@@ -43,40 +31,60 @@ const AddRestaurant = ({ auth }) => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const validate = (e) => {
     e.preventDefault();
+    let errors = {};
 
-    if (
-      restaurantForm.name !== '' &&
-      restaurantForm.website !== '' &&
-      restaurantForm.postcode !== '' &&
-      restaurantForm.comment !== '' &&
-      restaurantForm.cuisine !== '' &&
-      restaurantForm.city !== '' &&
-      restaurantForm.budget.length !== 0 &&
-      mustEat.meOne !== '' &&
-      mustEat.meTwo !== '' &&
-      mustEat.meThree !== ''
-    ) {
-      postData();
-      setError(false);
+    if (!restaurantForm.name) {
+      errors.name = 'you must enter a name!';
     }
-    setError(true);
+    if (!restaurantForm.website) {
+      errors.website = 'you must enter a website!';
+    }
+    if (!restaurantForm.postcode) {
+      errors.postcode = 'you must enter a postcode!';
+    }
+    if (!restaurantForm.comment) {
+      errors.comment = 'you must enter a comment!';
+    }
+    if (!restaurantForm.cuisine) {
+      errors.cuisine = true;
+    }
+    if (!restaurantForm.budget) {
+      errors.budget = true;
+    }
+    if (!restaurantForm.city) {
+      errors.city = true;
+    }
+    if (!mustEat.meOne) {
+      errors.meOne = 'you must enter a must eat!';
+    }
+    if (!mustEat.meTwo) {
+      errors.meTwo = 'you must enter a must eat!';
+    }
+    if (!mustEat.meThree) {
+      errors.meThree = 'you must enter a must eat!';
+    }
+    if (Object.keys(errors).length) {
+      return setError(errors);
+    }
+    if (!Object.keys(errors).length) {
+      setError({});
+      postData();
+    }
   };
 
   const postData = async () => {
     const mustEats = [mustEat.meOne, mustEat.meTwo, mustEat.meThree];
     const { data } = await addRestaurant(restaurantForm, mustEats);
     if (data) {
-      setRestaurantForm({ name: '', website: '', postcode: '', comment: '', mustEats: [], cuisine: '', city: '', budget: [] });
-      setMustEat({
-        meOne: '',
-        meTwo: '',
-        meThree: '',
-      });
+      setRestaurantForm({});
+      setMustEat({});
       auth(false);
     }
   };
+
+  console.log(restaurantForm, mustEat, error);
 
   return (
     <div className='add-restaurant-container'>
@@ -84,15 +92,65 @@ const AddRestaurant = ({ auth }) => {
         <h1>Add new Restaurant</h1>
         <h2>Welcome Vanessa!</h2>
       </div>
-      <form autoComplete='off' onSubmit={handleSubmit}>
-        <input onChange={handleInputChange} autoComplete='off' placeholder='name' className={`name ${error && 'error'}`} type='text' name='name' />
-        <input onChange={handleInputChange} placeholder='website' className={`website ${error && 'error'}`} type='text' name='website' />
-        <input onChange={handleInputChange} placeholder='postcode' className={`postcode ${error && 'error'}`} type='text' name='postcode' />
-        <textarea onChange={handleInputChange} placeholder='comment' className={`comment ${error && 'error'}`} name='comment' cols='25' rows='3'></textarea>
-        <input onChange={handleInputChange} placeholder='must eat 1' className={`must-1 ${error && 'error'}`} type='text' name='meOne'></input>
-        <input onChange={handleInputChange} placeholder='must eat 2' className={`must-2 ${error && 'error'}`} type='text' name='meTwo'></input>
-        <input onChange={handleInputChange} placeholder='must eat 3' className={`must-3 ${error && 'error'}`} type='text' name='meThree'></input>
-        <select onChange={handleInputChange} className={`cuisine-select ${error && 'error'}`} name='cuisine'>
+      <form onSubmit={validate}>
+        <input
+          onChange={handleInputChange}
+          value={restaurantForm.name || ''}
+          placeholder={!error.name ? 'name' : error.name}
+          className={`name ${error.name && 'error'}`}
+          type='text'
+          name='name'
+        />
+        <input
+          onChange={handleInputChange}
+          value={restaurantForm.website || ''}
+          placeholder={!error.website ? 'website' : error.website}
+          className={`website ${error.website && 'error'}`}
+          type='text'
+          name='website'
+        />
+        <input
+          onChange={handleInputChange}
+          value={restaurantForm.postcode || ''}
+          placeholder={!error.postcode ? 'postcode' : error.postcode}
+          className={`postcode ${error.postcode && 'error'}`}
+          type='text'
+          name='postcode'
+        />
+        <textarea
+          onChange={handleInputChange}
+          value={restaurantForm.comment || ''}
+          placeholder={!error.comment ? 'comment' : error.comment}
+          className={`comment ${error.comment && 'error'}`}
+          name='comment'
+          cols='25'
+          rows='3'
+        ></textarea>
+        <input
+          onChange={handleInputChange}
+          value={mustEat.meOne || ''}
+          placeholder={!error.meOne ? 'must eat 1' : error.meOne}
+          className={`must-1 ${error.meOne && 'error'}`}
+          type='text'
+          name='meOne'
+        ></input>
+        <input
+          onChange={handleInputChange}
+          value={mustEat.meTwo || ''}
+          placeholder={!error.meTwo ? 'must eat 2' : error.meTwo}
+          className={`must-2 ${error.meTwo && 'error'}`}
+          type='text'
+          name='meTwo'
+        ></input>
+        <input
+          onChange={handleInputChange}
+          value={mustEat.meThree || ''}
+          placeholder={!error.meThree ? 'must eat 3' : error.meThree}
+          className={`must-3 ${error.meThree && 'error'}`}
+          type='text'
+          name='meThree'
+        ></input>
+        <select onChange={handleInputChange} className={`cuisine-select ${error.cuisine && 'error'}`} name='cuisine'>
           <option disabled selected value='disabled'>
             choose cuisine
           </option>
@@ -107,14 +165,14 @@ const AddRestaurant = ({ auth }) => {
           <option value='drinks'>Drinks</option>
           <option value='misc.'>Misc.</option>
         </select>
-        <select onChange={handleInputChange} className={`city ${error && 'error'}`} name='city'>
+        <select onChange={handleInputChange} className={`city ${error.city && 'error'}`} name='city'>
           <option disabled selected value='disabled'>
             choose city
           </option>
           <option value='london'>London</option>
           <option value='singapore'>Singapore</option>
         </select>
-        <select onChange={handleInputChange} className={`budget ${error && 'error'}`} name='budget'>
+        <select onChange={handleInputChange} className={`budget ${error.budget && 'error'}`} name='budget'>
           <option disabled selected value='disabled'>
             choose cost
           </option>
